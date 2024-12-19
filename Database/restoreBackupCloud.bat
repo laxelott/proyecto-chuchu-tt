df91d6dec48bf3901a549184ea557b5c0e871837
Axel Treviño <laxelott@hotmail.com>
2024-12-19 09:23:35 -0600

    Database: Fixed cloud scripts

--- Database/restoreBackupCloud.bat
+++ Database/restoreBackupCloud.bat
@@ -1,14 +1,15 @@
 @echo off
 
 echo Reading credentials...
-for /f "delims=" %%A in (../password) do (
+for /f "delims=" %%A in (password) do (
     set "password=%%A"
 )
-for /f "delims=" %%A in (../host) do (
+for /f "delims=" %%A in (host) do (
     set "host=%%A"
 )
 if not defined password if not defined host (
     echo Error: Credential files not found!
+    pause
     exit /b 1
 )
 
--- Database/runScriptsCloud.bat
+++ Database/runScriptsCloud.bat
@@ -1,18 +1,19 @@
 @echo off
 
 echo Reading credentials...
-for /f "delims=" %%A in (../password) do (
+for /f "delims=" %%A in (password) do (
     set "password=%%A"
 )
-for /f "delims=" %%A in (../host) do (
+for /f "delims=" %%A in (host) do (
     set "host=%%A"
 )
 if not defined password if not defined host (
     echo Error: Credential files not found!
+    pause
     exit /b 1
 )
 
 echo Running...
-mysqlsh --user root --password %password% --host %host% --file chuchu_sproc.sql
+mysqlsh --user root --password %password% --host %host% --file "chuchu_sproc.sql"
 echo Done
 pause
